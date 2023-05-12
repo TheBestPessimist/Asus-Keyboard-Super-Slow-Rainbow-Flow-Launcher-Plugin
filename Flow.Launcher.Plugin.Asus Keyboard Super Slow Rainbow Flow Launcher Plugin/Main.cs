66821f9ce246386d9d5384369c0a977118bda22c
TheBestPessimist <cristian+gitpush@tbp.land>
2023-05-12 15:45:44 +0300

    It works!

--- Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/Main.cs
+++ Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/Main.cs
@@ -1,21 +1,72 @@
 using System;
 using System.Collections.Generic;
-using Flow.Launcher.Plugin;
+using System.Threading;
+using System.Threading.Tasks;
+using Asus_Keyboard_Super_Slow_Rainbow;
+using JetBrains.Annotations;
 
 namespace Flow.Launcher.Plugin.Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin
 {
-    public class Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin : IPlugin
+    /// <summary>
+    /// Plug used to make Asus ROG Aura Keyboards show a very slow rainbow. Much slower than the default Aura speed.
+    /// </summary>
+    [UsedImplicitly]
+    public class Main : IAsyncPlugin
     {
-        private PluginInitContext _context;
+        // private PluginInitContext _context;
 
-        public void Init(PluginInitContext context)
+        private RainbowColors _currentRainbow;
+        private Task _currentTask;
+
+        /// <inheritdoc />
+        public Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
-            _context = context;
+            var result = new List<Result>();
+            if (string.IsNullOrWhiteSpace(query.Search)) return Task.FromResult(result);
+
+            int newMinutesAmount;
+            if (int.TryParse(query.Search, out newMinutesAmount) && newMinutesAmount > 0)
+            {
+                result.Add(new Result
+                {
+                    Title = $"Desired duration: {newMinutesAmount} minutes",
+                    AutoCompleteText = "",
+                    Action = _ =>
+                    {
+                        _stopAndReload(newMinutesAmount);
+                        return true;
+                    }
+                });
+            }
+            else
+            {
+                result.Add(new Result
+                {
+                    Title = $"Desired duration is not a valid natural number.",
+                    AutoCompleteText = ""
+                });
+            }
+
+            return Task.FromResult(result);
         }
 
-        public List<Result> Query(Query query)
+        /// <inheritdoc />
+        public Task InitAsync(PluginInitContext context)
         {
-            return new List<Result>();
+            _stopAndReload(1);
+            return Task.CompletedTask;
+        }
+
+        private void _stopAndReload(int minutes)
+        {
+            if (_currentRainbow != null)
+            {
+                _currentRainbow.IsCancelRequested = true;
+                _currentTask.Wait();
+            }
+
+            _currentRainbow = new RainbowColors(TimeSpan.FromMinutes(minutes));
+            _currentTask = Task.Run(() => _currentRainbow.DoTheRainbow());
         }
     }
 }
--- release.ps1
+++ release.ps1
@@ -1,2 +1,7 @@
-dotnet publish Flow.Launcher.Plugin.Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin -c Release -r win-x64 --no-self-contained
-Compress-Archive -LiteralPath Flow.Launcher.Plugin.Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin/bin/Asus_Keyboard_Super_Slow_Rainbow_Flow_Launcher_Plugin.zip -Force
+Stop-Process -Name "Flow.Launcher"
+
+dotnet publish "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin" -c Release -r win-x64 --no-self-contained
+Compress-Archive -LiteralPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/publish" -DestinationPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Asus Keyboard Super Slow Rainbow Flow Launcher Plugin.zip" -Force
+robocopy /E /Z /R:5 /W:5 /TBD /unicode /V /XJ /ETA /MT:32       'D:\all\work\Asus Keyboard Super Slow Rainbow Flow Launcher Plugin\Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin\bin\Release\win-x64\publish' 'C:\Users\TheBestPessimist\AppData\Roaming\FlowLauncher\Plugins\Asus-1.0.0'
+
+& "D:/all/work/Flow.Launcher/Output/Debug/Flow.Launcher.exe"
