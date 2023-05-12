898449546307ead563241a9303c920a091c0a945
TheBestPessimist <cristian+gitpush@tbp.land>
2023-05-12 19:48:31 +0300

    Move all the files around. Just because i barely got comfortable in this mess.

--- release.ps1
+++ release.ps1
@@ -1,12 +1,14 @@
 
 echo "➡️ dir: $pwd"
 
-$version = (Get-Content 'Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/plugin.json' | ConvertFrom-Json).Version
+$version = (Get-Content 'plugin.json' | ConvertFrom-Json).Version
 
-dotnet publish "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin" -c Release -r win-x64 --no-self-contained
+dotnet publish  -c Release -r win-x64 --no-self-contained
+
+
 Remove-Item -Path "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/Asus Keyboard Super Slow Rainbow-$version" -Recurse -Force -ErrorAction SilentlyContinue
-Rename-Item -Path "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/publish" -NewName "Asus Keyboard Super Slow Rainbow-$version" -Force
-Compress-Archive -LiteralPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/Asus Keyboard Super Slow Rainbow-$version" -DestinationPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Asus Keyboard Super Slow Rainbow Flow Launcher Plugin-$version.zip" -Force
+#Rename-Item -Path "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/publish" -NewName "Asus Keyboard Super Slow Rainbow-$version" -Force
+#Compress-Archive -LiteralPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Release/win-x64/Asus Keyboard Super Slow Rainbow-$version" -DestinationPath "Flow.Launcher.Plugin.Asus Keyboard Super Slow Rainbow Flow Launcher Plugin/bin/Asus Keyboard Super Slow Rainbow Flow Launcher Plugin-$version.zip" -Force
 
 
 ### optional steps
