2921b556cc697000361476130d03915d711f4e67
TheBestPessimist <cristian+gitpush@tbp.land>
2023-05-23 14:54:12 +0200

    Add the `exe` project to this solution

---
+++ Asus Keyboard Super Slow Rainbow/src/Aura.cs
@@ -0,0 +1,80 @@
+﻿using System.Drawing;
+using HidLibrary;
+using static System.Console;
+
+namespace Asus_Keyboard_Super_Slow_Rainbow;
+
+// Stolen from https://github.com/JamesCJ60/Armoury-Control/blob/master/acControl/Scripts/Aura.cs
+//
+// This is an optimised/simplified version of Aura.cs from https://github.com/seerge/g-helper
+// I do not take credit for the full functionality of the code.
+//
+
+public static class Aura
+{
+    // ReSharper disable once InconsistentNaming
+    private static readonly byte[] MESSAGE_SET = { 0x5d, 0xb5, 0, 0, 0 };
+
+    // ReSharper disable once InconsistentNaming
+    private static readonly byte[] MESSAGE_APPLY = { 0x5d, 0xb4, 0, 0, 0 };
+
+    private const int ModeStatic = 0;
+
+    private const int SpeedSlow = 0;
+
+    private static readonly List<HidDevice> AuraDevices = new();
+
+    static Aura()
+    {
+        _refreshAuraDevices();
+    }
+
+    private static void _refreshAuraDevices()
+    {
+        int[] deviceIds = { 0x1854, 0x1869, 0x1866, 0x19b6, 0x1822, 0x1837, 0x1854, 0x184a, 0x183d, 0x8502, 0x1807, 0x17e0 };
+        foreach (var device in HidDevices.Enumerate(0x0b05, deviceIds))
+        {
+            device.OpenDevice();
+            if (device.Write(MESSAGE_SET))
+            {
+                AuraDevices.Add(device);
+            }
+
+            device.CloseDevice();
+        }
+
+        WriteLine($"AuraDevices:");
+        foreach (var d in AuraDevices)
+        {
+            Write($"    {d}, ");
+        }
+    }
+
+    public static void ApplyAura(Color color)
+    {
+        // MeasureDuration(() => _applyAura(color));
+        _applyAura(color);
+    }
+
+    private static void _applyAura(Color color)
+    {
+        foreach (var device in AuraDevices)
+        {
+            if (!device.IsOpen)
+            {
+                device.OpenDevice();
+            }
+
+            byte[] msg =
+            {
+                0x5d, 0xb3, 0x00, ModeStatic,
+                color.R, color.G, color.B,
+                SpeedSlow, 0, 0,
+                color.R, color.G, color.B
+            };
+            device.Write(msg);
+            device.Write(MESSAGE_SET);
+            device.Write(MESSAGE_APPLY);
+        }
+    }
+}
---
+++ Asus Keyboard Super Slow Rainbow/src/RainbowColors.cs
@@ -0,0 +1,77 @@
+using System.Drawing;
+
+namespace Asus_Keyboard_Super_Slow_Rainbow;
+
+public class RainbowColors
+{
+    private readonly int _step;
+
+    private readonly int _iterations;
+
+    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+    private readonly TimeSpan _desiredDuration;
+    private readonly TimeSpan _sleepBetweenIterations;
+
+    private readonly CancellationToken _ct;
+
+    public RainbowColors(TimeSpan desiredDuration, int step = 10, CancellationToken ct = default)
+    {
+        _desiredDuration = desiredDuration;
+        _sleepBetweenIterations = desiredDuration / _iterations;
+        _step = step;
+        _iterations = 256 * 6 / _step;
+        _ct = ct;
+
+        Console.WriteLine($"""
+            Number of colors changed in 1 rainbow cycle: {_iterations}.
+            Desired Duration: {_desiredDuration}.
+            Sleep between each color change: {_sleepBetweenIterations}.
+            """);
+    }
+
+
+    // ReSharper disable once FunctionNeverReturns
+    public void DoTheRainbow()
+    {
+        while (true)
+        {
+            if (_ct.IsCancellationRequested) return;
+
+            // ### Red ➡️ Orange ➡️ Yellow
+            Console.WriteLine("Red ➡️ Orange ➡️ Yellow");
+            for (int green = 0; green <= 255; green += _step) SetRgb(255, green, 0);
+
+            // ### Yellow ➡️ Green
+            Console.WriteLine("Yellow ➡️ Green");
+            for (int red = 255; red >= 0; red -= _step) SetRgb(red, 255, 0);
+
+            // ### Green ➡️ Cyan
+            Console.WriteLine("Green ➡️ Cyan");
+            for (int blue = 0; blue <= 255; blue += _step) SetRgb(0, 255, blue);
+
+            // ### Cyan ➡️ Blue
+            Console.WriteLine("Cyan ➡️ Blue");
+            for (int green = 255; green >= 0; green -= _step) SetRgb(0, green, 255);
+
+            // ### Blue ➡️ Purple
+            Console.WriteLine("Blue ➡️ Purple");
+            for (int red = 0; red <= 255; red += _step) SetRgb(red, 0, 255);
+
+            // ### Purple ➡️ Red
+            Console.WriteLine("Purple ➡️ Red");
+            for (int blue = 255; blue >= 0; blue -= _step) SetRgb(255, 0, blue);
+
+            Console.WriteLine("============== Restart");
+        }
+    }
+
+
+    private void SetRgb(int r, int g, int b)
+    {
+        if (_ct.IsCancellationRequested) return;
+
+        Aura.ApplyAura(Color.FromArgb(r, g, b));
+        Console.WriteLine($"{r} {g} {b}");
+        _ct.WaitHandle.WaitOne(_sleepBetweenIterations);
+    }
+}
